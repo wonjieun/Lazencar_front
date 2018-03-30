b24e2e9c28d3bc29ac32e2c92bf18bb4109bbd73
영기이이 <@192.168.30.150>
2018-03-30 14:20:24 +0900

    후기 게시판, 사이드 메뉴

---
+++ src/controller/ReviewList.java
@@ -0,0 +1,52 @@
+package controller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.ReviewDao;
+import dao.ReviewDaoImpl;
+import dto.Paging;
+import dto.Review;
+
+@WebServlet("/Page/review_Board_List.do")
+public class ReviewList extends HttpServlet {
+	
+	private ReviewDao dao = new ReviewDaoImpl();
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doPost(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Paging paging = null;	// 페이징 객체
+		List<Review> list = null;	// 게시물 리스트
+		
+		// 요청파라미터에서 페이징 번호 얻기
+		String pageParam = req.getParameter("pageNo");
+		int pageNo = 0;
+		if(pageParam != null)	pageNo = Integer.parseInt(pageParam);
+	
+		// 페이징 로직 처리
+		int totalCount = dao.getTotal();	// 총 게시물 수
+		if(totalCount != 0) {
+			paging = new Paging(totalCount, pageNo);	// 총 게시물수와 페이지번호를 이용한 페이징 객체 생성
+			list = dao.getList(paging);	// 페이지에 맞는 게시물 가져오기
+		}
+
+		// 페이징 정보와 게시물 정보 모델로써 전달하기 
+		req.setAttribute("paging", paging);
+		req.setAttribute("list", list);
+		
+		// list 페이지 불러오기
+		req.getRequestDispatcher("/Page/review_Board_List.jsp").forward(req, resp);
+	}
+
+}
---
+++ src/dto/Review.java
@@ -0,0 +1,65 @@
+package dto;
+
+public class Review {
+	
+	private int rev_num;
+	private String rev_title;
+	private String rev_userId;
+	private String rev_content;
+	private String rev_date;
+	private int rev_views;
+	private int rev_hit;
+	
+	public int getRev_views() {
+		return rev_views;
+	}
+	public void setRev_views(int rev_views) {
+		this.rev_views = rev_views;
+	}
+	private int rev_avail;
+	
+	public int getRev_avail() {
+		return rev_avail;
+	}
+	public void setRev_avail(int rev_avail) {
+		this.rev_avail = rev_avail;
+	}
+	public int getRev_num() {
+		return rev_num;
+	}
+	public void setRev_num(int rev_num) {
+		this.rev_num = rev_num;
+	}
+	public String getRev_title() {
+		return rev_title;
+	}
+	public void setRev_title(String rev_title) {
+		this.rev_title = rev_title;
+	}
+	public String getRev_userId() {
+		return rev_userId;
+	}
+	public void setRev_userId(String rev_userId) {
+		this.rev_userId = rev_userId;
+	}
+	public String getRev_content() {
+		return rev_content;
+	}
+	public void setRev_content(String rev_content) {
+		this.rev_content = rev_content;
+	}
+	public String getRev_date() {
+		return rev_date;
+	}
+	public void setRev_date(String rev_date) {
+		this.rev_date = rev_date;
+	}
+	public int getRev_hit() {
+		return rev_hit;
+	}
+	public void setRev_hit(int rev_hit) {
+		this.rev_hit = rev_hit;
+	}
+
+
+}
